db3deb4e34763454ecb863b0d3bc6069e203e2a2
Leonardo Pereira <loen.ribas20@gmail.com>
2023-07-25 09:14:03 -0400

    Método calcular diferença adicionado

---
+++ Método Somar/Calcular Diferença/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcular_Diferença
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe o 1º valor: ");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe o 2º valor: ");
+            double y = double.Parse(Console.ReadLine());
+
+            double resultado = CalcularDiferenca(x, y);
+            Console.WriteLine("O resultado é " + resultado);
+            Console.ReadKey();
+        }
+        static double CalcularDiferenca(double x, double y)
+        {
+            double resultado;
+            if (x >= y)
+            {
+                resultado = x - y;
+            }
+            if (y > x)
+            {
+                resultado = y - x;
+            }
+            return resultado;
+        }
+    }
+}
--- Método Somar/Método Somar/Program.cs
+++ Método Somar/Método Somar/Program.cs
@@ -10,6 +10,10 @@ namespace Método_Somar
     {
         static void Main(string[] args)
         {
+            Exercicio1();
+        }
+        static void Exercicio1()
+        {
             Console.Write("Informe o 1º valor: ");
             double x = double.Parse(Console.ReadLine());
 
@@ -20,9 +24,26 @@ namespace Método_Somar
             Console.WriteLine("O resultado é " + resultado);
             Console.ReadKey();
         }
+        static void Exercicio2()
+        {
+            Console.Write("Informe o 1º valor: ");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe o 2º valor: ");
+            double y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nDiferença: " + CalcularDiferenca(x, y));
+        }
         static double Somar(double x, double y)
         {
             return x + y;
+        }
+        static double CalcularDiferenca(double x, double y)
+        {
+            if (x > y)
+                return x - y;
+            else
+                return y - x;
         }
     }
 }
