1749f8ba5ac71facac2cc8c77391e90c5bf659d1
Leonardo Pereira <loen.ribas20@gmail.com>
2023-07-25 08:59:07 -0400

    Exercício do método somar solucionado

--- Método Somar/Método Somar/Program.cs
+++ Método Somar/Método Somar/Program.cs
@@ -10,6 +10,19 @@ namespace Método_Somar
     {
         static void Main(string[] args)
         {
+            Console.Write("Informe o 1º valor: ");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe o 2º valor: ");
+            double y = double.Parse(Console.ReadLine());
+
+            double resultado = Somar(x, y);
+            Console.WriteLine("O resultado é " + resultado);
+            Console.ReadKey();
+        }
+        static double Somar(double x, double y)
+        {
+            return x + y;
         }
     }
 }
