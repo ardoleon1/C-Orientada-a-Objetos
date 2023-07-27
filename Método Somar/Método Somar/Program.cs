ed78416e9de6388bc76d4d7628ed77eeffdb905e
Leonardo Pereira <loen.ribas20@gmail.com>
2023-07-27 14:20:38 -0400

    Todos os exercícios postado

--- Método Somar/Método Somar/Program.cs
+++ Método Somar/Método Somar/Program.cs
@@ -11,9 +11,13 @@ namespace Método_Somar
         static void Main(string[] args)
         {
             Exercicio1();
+            Exercicio2();
+            Exercicio3();
+            Console.ReadKey();
         }
         static void Exercicio1()
         {
+            Console.WriteLine("\t\t---Exercício 1---");
             Console.Write("Informe o 1º valor: ");
             double x = double.Parse(Console.ReadLine());
 
@@ -22,10 +26,10 @@ namespace Método_Somar
 
             double resultado = Somar(x, y);
             Console.WriteLine("O resultado é " + resultado);
-            Console.ReadKey();
         }
         static void Exercicio2()
         {
+            Console.WriteLine("\t\t---Exercício 2---");
             Console.Write("Informe o 1º valor: ");
             double x = double.Parse(Console.ReadLine());
 
@@ -33,6 +37,18 @@ namespace Método_Somar
             double y = double.Parse(Console.ReadLine());
 
             Console.WriteLine("\nDiferença: " + CalcularDiferenca(x, y));
+        }
+        static void Exercicio3()
+        {
+            Console.WriteLine("\t\t---Exercício 3---");
+            Console.Write("Informe o x: ");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe o y: ");
+            double y = double.Parse(Console.ReadLine());
+
+            Inverter(ref x, ref y);
+            Console.WriteLine("\nInvertendo\nx: " + x + "\ny: " + y);
         }
         static double Somar(double x, double y)
         {
@@ -44,6 +60,12 @@ namespace Método_Somar
                 return x - y;
             else
                 return y - x;
+        }
+        static void Inverter(ref double x, ref double y)
+        {
+            double aux = x;
+            x = y;
+            y = aux;
         }
     }
 }
